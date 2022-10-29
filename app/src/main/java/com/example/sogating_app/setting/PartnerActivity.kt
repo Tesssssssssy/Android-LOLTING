03a3c5ccd0e41670f8ab96e947d6f5c9a37291bf
dornsms777 <95116297+dornsms777@users.noreply.github.com>
2022-10-29 22:17:30 +0900

    parteractivity 구색만 만듬

--- app/src/main/java/com/example/sogating_app/setting/PartnerActivity.kt
+++ app/src/main/java/com/example/sogating_app/setting/PartnerActivity.kt
@@ -3,7 +3,9 @@ package com.example.sogating_app.setting
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.sogating_app.MAIN.MainActivity
@@ -19,6 +21,18 @@ class PartnerActivity : AppCompatActivity() {
         back_button.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+        }
+
+        //친구와 함께 버튼 클릭
+        var friendbtn = findViewById<Button>(R.id.friendbtn)
+        friendbtn.setOnClickListener{
+            Toast.makeText(this, "동성 친구와 매칭 됩니다!", Toast.LENGTH_SHORT).show()
+        }
+
+        //이성과 함께 버튼 클릭
+        var loverbtn = findViewById<Button>(R.id.loverbtn)
+        loverbtn.setOnClickListener{
+            Toast.makeText(this, "이성 친구와 매칭 됩니다!", Toast.LENGTH_SHORT).show()
         }
     }
 
