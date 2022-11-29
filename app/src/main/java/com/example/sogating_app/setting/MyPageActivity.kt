bef12d0c1ec8fb095cbb3a9a15c1922ae1771244
JUNGCHAEYOON <dornsms888@gmail.com>
2022-11-30 02:10:31 +0900

    주소 살짝 변경 서울: 대한민국 서울특별시 ~구 이외의 지역: 대한민국 경기도 고양시 ~구

--- app/src/main/java/com/example/sogating_app/setting/MyPageActivity.kt
+++ app/src/main/java/com/example/sogating_app/setting/MyPageActivity.kt
@@ -434,7 +434,11 @@ class MyPageActivity : AppCompatActivity() {
 
         mylocation = LatLng(mLastLocation.latitude, mLastLocation.longitude) // mylocation 변수 저장
         var buff = getAddress(mylocation).split(" ")
-        myCity.text = buff[0] + " " + buff[1] + " " + buff[2]  //내주소
+        if(buff[1] == "서울특별시"){
+            myCity.text = buff[0] + " " + buff[1] + " " + buff[2]  //내주소
+        }else{
+            myCity.text = buff[0] + " " + buff[1] + " " + buff[2] + " " + buff[3]  //내주소
+        }
         FirebaseRef.userInfoRef.child(uid).child("city").setValue(myCity.text)
     }
 
